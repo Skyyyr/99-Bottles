08d6ef251131d85c069a13ddc62c5da0392a0e41
Skyyyr <emuswgluvr@gmail.com>
2022-05-23 14:34:09 -0700

    Semi cooolant.... lol.

--- 99_bottles.js
+++ 99_bottles.js
@@ -3,7 +3,7 @@ const finishedMessage = "Take one down and pass it around, 1 bottle of beer on t
 "1 bottle of beer on the wall, 1 bottle of beer.\n" +
 "Take one down and pass it around, no more bottles of beer on the wall.\n" +
 "No more bottles of beer on the wall, no more bottles of beer.\n" +
-"Go to the store and buy some more, 99 bottles of beer on the wall."
+"Go to the store and buy some more, 99 bottles of beer on the wall.";
 
 function bottleSong(bottleNumber = 99) {
   // Write your code here!
