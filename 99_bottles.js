b5d88bcc0f0b4cf4da352a83d960f5299133a49c
Skyyyr <emuswgluvr@gmail.com>
2022-05-23 13:32:29 -0700

    First 99 bottles

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,6 +1,19 @@
-function bottleSong() {
+//Just because... initialize it once and use it at the end.
+const finishedMessage = "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
+"1 bottle of beer on the wall, 1 bottle of beer.\n" +
+"Take one down and pass it around, no more bottles of beer on the wall.\n" +
+"No more bottles of beer on the wall, no more bottles of beer.\n" +
+"Go to the store and buy some more, 99 bottles of beer on the wall."
+
+function bottleSong(bottleNumber = 99) {
   // Write your code here!
-  //Many tests!
+  const output = bottleNumber + " Bottles of beer on the wall, " + bottleNumber + " bottles of beer on the wall.\n"
+  + "Take one down pass it around, " + (bottleNumber - 1) + " on the wall.";
+  if (bottleNumber === 1) {
+    return console.log(finishedMessage);
+  }
+  console.log(output);
+  bottleSong(bottleNumber - 1);
 };
 
 bottleSong();
