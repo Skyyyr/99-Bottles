031ca9a47ad5a003a570ee7054fe1e97fcb81805
Skyyyr <emuswgluvr@gmail.com>
2022-05-25 10:57:38 -0700

    Python 99 bottles, minor updates to js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,14 +1,10 @@
 //Just because... initialize it once and use it at the end.
-const finishedMessage = "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
-"1 bottle of beer on the wall, 1 bottle of beer.\n" +
-"Take one down and pass it around, no more bottles of beer on the wall.\n" +
-"No more bottles of beer on the wall, no more bottles of beer.\n" +
-"Go to the store and buy some more, 99 bottles of beer on the wall.";
+const finishedMessage = "Take one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\Take one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
 
 function bottleSong(bottleNumber = 99) {
   // Write your code here!
   const output = bottleNumber + " Bottles of beer on the wall, " + bottleNumber + " bottles of beer on the wall.\n"
-  + "Take one down pass it around, " + (bottleNumber - 1) + " on the wall.";
+  + "Take one down pass it around, " + (bottleNumber - 1) + " more on the wall.";
   if (bottleNumber === 1) {
     return console.log(finishedMessage);
   }
--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,10 @@
-def bottle_song():
+finished_message = "Take one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+def bottle_song(bottle_number):
 	# write your code here!
+	output = f"{bottle_number} Bottles of beer on the wall, {bottle_number} bottles of beer on the wall.\nTake one down pass it around, {bottle_number - 1} more on the wall.";
+	if (bottle_number == 1):
+		return print(finished_message);
+	print(output)
+	bottle_song(bottle_number - 1)
 
-
-bottle_song()
+bottle_song(99)
